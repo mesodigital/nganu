d57476a2b11405bb566e7033f4b9ba282eac769a
mesodigital <ghostdsgn404@gmail.com>
2024-08-01 16:03:19 +0700

    s

--- y.sh
+++ y.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 
+adb_device=""
+adb(){
+    if [ -n "$adb_device" ]; then
+        adb -s "$adb_device" shell "$@"
+        return $?
+    else
+        "$@"
+        return $?
+    fi
+}
